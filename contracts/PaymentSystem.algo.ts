8a2c7ac33dc8153f5dae8cc5ea67bb7479516613
nullun <git@nullun.com>
2023-11-15 14:54:04 +0000

    Initial Payment System contracts
    TODO: Add jest test rather than test.sh

---
+++ contracts/PaymentSystem.algo.ts
@@ -0,0 +1,329 @@
+import { Contract } from '@algorandfoundation/tealscript';
+
+// Key to use per partner and owner (user)
+type PartnerAndOwner = {
+    partner: string,
+    owner: Address
+};
+
+// Withdrawal request for an amount of an asset, where the round indicates the earliest it can be made
+type WithdrawalRequest = {
+    nonce: uint64,
+    round: uint64,
+    asset: Asset,
+    amount: uint64
+};
+
+// Cost of storing (partner + owner) + card in a box
+const box_mbr: uint64 = (2500) + (400 * (64 + 32));
+
+// Cost of new card
+const card_mbr: uint64 = globals.minBalance + box_mbr;
+
+// Cost of added an asset to the card account
+// TODO: Replace with assetOptInMinBalance (V10)
+const asset_mbr: uint64 = 100000;
+
+class Card extends Contract {
+    /**
+     * Create a new account, rekeying it to the caller application address
+     * @returns New account address
+     */
+    @allow.create("DeleteApplication")
+    new(): Address {
+        sendPayment({
+            receiver: this.app.address,
+            amount: 0,
+            rekeyTo: globals.callerApplicationAddress,
+        });
+
+        return this.app.address;
+    }
+}
+
+class PaymentSystem extends Contract {
+    // Admin
+    admin = GlobalStateKey<Address>({ key: 'a' });
+
+    // Owner and Card
+    cards = BoxMap<PartnerAndOwner, Address>({});
+    active_cards = GlobalStateKey<uint64>({ key: 'c' });
+
+    // Rounds to wait
+    withdrawal_wait_time = GlobalStateKey<uint64>({ key: 'w' });
+
+    // Withdrawal requests
+    withdrawals = LocalStateMap<byte[32], WithdrawalRequest>({ maxKeys: 15});
+    withdrawal_nonce = LocalStateKey<uint64>({ key: 'n' });
+
+    /**
+     * Check if the current transaction sender is the admin
+     * @returns True if the sender is the admin
+     */
+    private is_admin(): boolean {
+        return this.txn.sender === this.admin.value;
+    }
+
+    /**
+     * Check if the current transaction sender is the owner of the card account
+     * @param card Address to check
+     * @returns True if the sender is the owner of the card
+     */
+    private is_owner(partner: string, card: Address): boolean {
+        return this.cards({partner: partner, owner: this.txn.sender} as PartnerAndOwner).value === card;
+    }
+
+    /**
+     * Deploy the smart contract, setting the transaction sender as the admin
+     */
+    @allow.create("NoOp")
+    deploy(): void {
+        this.admin.value = this.txn.sender;
+    }
+
+    /**
+     * Allows the admin to update the smart contract
+     */
+    @allow.call("UpdateApplication")
+    update(): void {
+        assert(this.is_admin());
+    }
+
+    /**
+     * Destroy the smart contract, sending all Algo to the admin account. This can only be done if there are no active cards
+     */
+    @allow.call("DeleteApplication")
+    destroy(): void {
+        assert(this.is_admin());
+
+        // There must not be any active cards
+        assert(!this.active_cards.value);
+
+        sendPayment({
+            receiver: this.app.address,
+            amount: 0,
+            closeRemainderTo: this.admin.value,
+        });
+    }
+
+    /**
+     * Allows the current admin to set a new admin
+     * @param admin Address to be made admin
+     */
+    setAdmin(admin: Address): void {
+        assert(this.is_admin());
+
+        this.admin.value = admin;
+    }
+
+    /**
+     * Set the number of rounds a withdrawal request must wait until being withdrawn
+     * @param rounds New number of rounds to wait
+     */
+    setWithdrawalRounds(rounds: uint64): void {
+        assert(this.is_admin());
+
+        this.withdrawal_wait_time.value = rounds;
+    }
+
+    /**
+     * Create account. This generates a brand new account and funds the minimum balance requirement
+     * @param owner Address to have control over asset withdrawals
+     * @returns Newly generated account used by their card
+     */
+    cardCreate(mbr: PayTxn, partner: string, owner: Address): Address {
+        assert(this.is_admin());
+
+        assert(mbr.amount === card_mbr);
+        assert(mbr.receiver === this.app.address);
+
+        // Create a new account
+        const card_addr: Address = sendMethodCall<[], Address>({
+            name: "new",
+            onCompletion: "DeleteApplication",
+            approvalProgram: Card.approvalProgram(),
+            clearStateProgram: Card.clearProgram(),
+        });
+
+        // Fund the account with a minimum balance
+        sendPayment({
+            receiver: card_addr,
+            amount: globals.minBalance,
+        });
+
+        // Store new card along with owner
+        this.cards({ partner: partner, owner: owner} as PartnerAndOwner).value = card_addr;
+
+        // Increment active cards
+        this.active_cards.value = this.active_cards.value + 1;
+
+        // Return the new account address
+        return card_addr;
+    }
+
+    /**
+     * Close account. This permanently removes the rekey and deletes the account from the ledger
+     * @param partner Partner name
+     * @param owner Address which has control over asset withdrawals
+     * @param card Account to close
+     */
+    cardClose(partner: string, owner: Address, card: Account): void {
+        assert(this.is_admin());
+
+        sendPayment({
+            sender: card,
+            receiver: card,
+            amount: 0,
+            closeRemainderTo: this.txn.sender,
+        });
+
+        sendPayment({
+            receiver: this.txn.sender,
+            amount: box_mbr,
+        })
+
+        // Delete the card from the box
+        this.cards({partner: partner, owner: owner} as PartnerAndOwner).delete();
+
+        // Decrement active cards
+        this.active_cards.value = this.active_cards.value - 1;
+    }
+
+    /**
+     * Allows the owner (or admin) to OptIn to an asset, increasing the minimum balance requirement of the account
+     * @param partner Partner name
+     * @param card Account to add asset to
+     * @param asset Asset to add
+     */
+    cardAddAsset(mbr: PayTxn, partner: string, card: Account, asset: Asset): void {
+        assert(this.is_admin() || this.is_owner(partner, card));
+
+        assert(mbr.amount === asset_mbr);
+        assert(mbr.receiver === this.app.address);
+
+        // TODO: Replace minBalance with assetOptInMinBalance (V10)
+        sendPayment({
+            receiver: card,
+            amount: globals.minBalance,
+        });
+
+        sendAssetTransfer({
+            sender: card,
+            assetReceiver: card,
+            xferAsset: asset,
+            assetAmount: 0,
+        });
+    }
+
+    /**
+     * Allows the owner (or admin) to CloseOut of an asset, reducing the minimum balance requirement of the account
+     * @param partner Partner name
+     * @param card Account to remove asset from
+     * @param asset Asset to remove
+     */
+    cardRemoveAsset(partner: string, card: Account, asset: Asset): void {
+        assert(this.is_admin() || this.is_owner(partner, card));
+
+        sendAssetTransfer({
+            sender: card,
+            assetReceiver: card,
+            assetCloseTo: card,
+            xferAsset: asset,
+            assetAmount: 0,
+        });
+
+        // TODO: Replace minBalance with assetOptInMinBalance (V10)
+        sendPayment({
+            sender: card,
+            receiver: this.txn.sender,
+            amount: globals.minBalance,
+        });
+    }
+
+    /**
+     * Allows the admin to send an amount of assets from the account
+     * @param card Account to debit from
+     * @param recipient Receiver of the assets being debited
+     * @param asset Asset being debited
+     * @param amount Amount to debit
+     */
+    cardDebit(card: Account, recipient: Account, asset: Asset, amount: uint64): void {
+        assert(this.is_admin());
+
+        sendAssetTransfer({
+            sender: card,
+            assetReceiver: recipient,
+            xferAsset: asset,
+            assetAmount: amount,
+        });
+    }
+
+    /**
+     * Allows the owner to send an amount of assets from the account
+     * @param partner Partner name
+     * @param card Account to withdraw from
+     * @param asset Asset being withdrawn
+     * @param amount Amount to withdraw
+     * @returns Withdrawal hash used for completing or cancelling the withdrawal
+     */
+    @allow.call("NoOp")
+    @allow.call("OptIn")
+    cardWithdrawalRequest(partner: string, card: Account, asset: Asset, amount: uint64): byte[32] {
+        assert(this.is_owner(partner, card));
+
+        const withdrawal: WithdrawalRequest = {
+            nonce: this.withdrawal_nonce(this.txn.sender).value,
+            round: globals.round + this.withdrawal_wait_time.value,
+            asset: asset,
+            amount: amount,
+        };
+        this.withdrawal_nonce(this.txn.sender).value = this.withdrawal_nonce(this.txn.sender).value + 1;
+        const withdrawal_hash: byte[32] = sha256(withdrawal as unknown as bytes);
+
+        this.withdrawals(
+            this.txn.sender,
+            withdrawal_hash,
+        ).value = withdrawal;
+
+        return withdrawal_hash;
+    }
+
+    /**
+     * Allows the owner (or admin) to cancel a withdrawal request
+     * @param partner Partner name
+     * @param card Account to withdraw from
+     * @param withdrawal_hash Hash of the withdrawal request
+     */
+    cardWithdrawalCancel(partner: string, card: Account, withdrawal_hash: byte[32]): void {
+        assert(this.is_admin() || this.is_owner(partner, card));
+
+        this.withdrawals(this.txn.sender, withdrawal_hash).delete();
+    }
+
+    /**
+     * Allows the owner to send an amount of assets from the account
+     * @param partner Partner name
+     * @param card Account to withdraw from
+     * @param recipient Receiver of the assets being withdrawn
+     * @param asset Asset being withdrawn
+     * @param withdrawal_hash Hash of the withdrawal request
+     */
+    @allow.call("NoOp")
+    @allow.call("CloseOut")
+    cardWithdraw(partner: string, card: Account, recipient: Account, asset: Asset, withdrawal_hash: byte[32]): void {
+        assert(this.is_owner(partner, card));
+
+        const withdrawal: WithdrawalRequest = this.withdrawals(this.txn.sender, withdrawal_hash).value as WithdrawalRequest;
+
+        assert(globals.round >= withdrawal.round);
+
+        sendAssetTransfer({
+            sender: card,
+            assetReceiver: recipient,
+            xferAsset: withdrawal.asset,
+            assetAmount: withdrawal.amount,
+        });
+
+        this.withdrawals(this.txn.sender, withdrawal_hash).delete();
+    }
+}
---
+++ test.sh
@@ -0,0 +1,210 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [ -z "$ALGORAND_DATA" ]; then
+	echo "ALGORAND_DATA environment variable is not set. Exiting."
+	exit 1
+fi
+
+GOAL="goal"
+
+ACCT1=$(${GOAL} account list | head -n 1 | tail -n 1 | awk '{print $3}' | tr -d '[\n\r]')
+ACCT2=$(${GOAL} account list | head -n 2 | tail -n 1 | awk '{print $3}' | tr -d '[\n\r]')
+
+APP_ID=$(${GOAL} app method \
+	--create \
+	-f ${ACCT1} \
+	--method "deploy()void" \
+	--approval-prog contracts/artifacts/PaymentSystem.approval.teal \
+	--clear-prog contracts/artifacts/PaymentSystem.clear.teal \
+	--global-byteslices 1 \
+	--global-ints 2 \
+	--local-byteslices 15 \
+	--local-ints 1 \
+	| grep 'Created app with app index' \
+	| awk '{print $6}' \
+	| tr -d '[\n\r]')
+
+APP_ADDR=$(${GOAL} app info \
+	--app-id ${APP_ID} \
+	| grep 'Application account' \
+	| awk '{print $3}' \
+	| tr -d '[\n\r]')
+
+# Fund minimum balance requirement
+${GOAL} clerk send -f ${ACCT1} -t ${APP_ADDR} -a 100000
+
+# Set withdrawal rounds
+${GOAL} app method \
+	-f ${ACCT1} \
+	--app-id ${APP_ID} \
+	--method "setWithdrawalRounds(uint64)void" \
+	--arg 5
+
+# Create a new card, providing the owner address
+${GOAL} clerk send \
+	-f ${ACCT1} \
+	-t ${APP_ADDR} \
+	-a 140900 \
+	-o mbr.txn
+#-a 128100 \
+CARD_ADDR=$(${GOAL} app method \
+	-f ${ACCT1} \
+	--app-id ${APP_ID} \
+	--method 'cardCreate(pay,string,address)address' \
+	--arg mbr.txn \
+	--arg '"Master Card"' \
+	--arg \"${ACCT2}\" \
+	--box "b64:ACILghwHcB9gaGvkDg56T+n/iWuorpF01uMYR4jgQ6PbNQALTWFzdGVyIENhcmQ=" \
+	--fee 4000 \
+	| grep 'method cardCreate(pay,string,address)address succeeded with output' \
+	| awk '{print $6}' \
+	| tr -d '[\n\r"]')
+#--box "addr:${ACCT2}" \
+
+# Create FakeUSDC ASA for testing
+FUSDC=$(${GOAL} asset create \
+	--creator ${ACCT1} \
+	--name "FakeUSDC" \
+	--unitname "FUSDC" \
+	--total 1000000000000 \
+	--decimals 6 \
+	| grep 'Created asset with asset index' \
+	| awk '{print $6}' \
+	| tr -d '[\r\n]')
+
+# OptIn and Send 100 FUSDC to ACCT2 (card owner)
+${GOAL} asset send \
+	--assetid ${FUSDC} \
+	-f ${ACCT2} \
+	-t ${ACCT2} \
+	-a 0
+${GOAL} asset send \
+	--assetid ${FUSDC} \
+	-f ${ACCT1} \
+	-t ${ACCT2} \
+	-a 100000000
+
+# Add FakeUSDC to card account
+${GOAL} clerk send \
+	-f ${ACCT1} \
+	-t ${APP_ADDR} \
+	-a 100000 \
+	-o mbr.txn
+${GOAL} app method \
+	--app-id ${APP_ID} \
+	-f ${ACCT1} \
+	--method "cardAddAsset(pay,string,account,asset)void" \
+	--arg mbr.txn \
+	--arg '"Master Card"' \
+	--arg ${CARD_ADDR} \
+	--arg ${FUSDC} \
+	--fee 3000
+
+# Deposit 10 FUSDC from owner to card
+${GOAL} asset send \
+	--assetid ${FUSDC} \
+	-f ${ACCT2} \
+	-t ${CARD_ADDR} \
+	-a 10000000
+
+# Owner spends $5 on his card, admin debits card
+${GOAL} app method \
+	--app-id ${APP_ID} \
+	-f ${ACCT1} \
+	--method "cardDebit(account,account,asset,uint64)void" \
+	--arg ${CARD_ADDR} \
+	--arg ${ACCT1} \
+	--arg ${FUSDC} \
+	--arg 5000000 \
+	--fee 2000
+
+# Owner creates withdraw request for 1 FUSDC
+WITHDRAWAL_HASH1=$(${GOAL} app method \
+	--app-id ${APP_ID} \
+	-f ${ACCT2} \
+	--method "cardWithdrawalRequest(string,account,asset,uint64)byte[32]" \
+	--on-completion "OptIn" \
+	--arg '"Master Card"' \
+	--arg ${CARD_ADDR} \
+	--arg ${FUSDC} \
+	--arg 1000000 \
+	--box "b64:ACILghwHcB9gaGvkDg56T+n/iWuorpF01uMYR4jgQ6PbNQALTWFzdGVyIENhcmQ=" \
+	--fee 2000 \
+	| grep 'method cardWithdrawalRequest(string,account,asset,uint64)byte\[32\] succeeded with output' \
+	| awk '{print $6}' \
+	| tr -d '[\n\r"]')
+
+# Owner creates withdraw request for remaining 4 FUSDC
+WITHDRAWAL_HASH2=$(${GOAL} app method \
+	--app-id ${APP_ID} \
+	-f ${ACCT2} \
+	--method "cardWithdrawalRequest(string,account,asset,uint64)byte[32]" \
+	--arg '"Master Card"' \
+	--arg ${CARD_ADDR} \
+	--arg ${FUSDC} \
+	--arg 4000000 \
+	--box "b64:ACILghwHcB9gaGvkDg56T+n/iWuorpF01uMYR4jgQ6PbNQALTWFzdGVyIENhcmQ=" \
+	--fee 2000 \
+	| grep 'method cardWithdrawalRequest(string,account,asset,uint64)byte\[32\] succeeded with output' \
+	| awk '{print $6}' \
+	| tr -d '[\n\r"]')
+
+echo "Wait at least 5 rounds to process withdrawals"
+read -p "Press enter to continue"
+
+# Owner withdraws 1 FUSDC
+${GOAL} app method \
+	--app-id ${APP_ID} \
+	-f ${ACCT2} \
+	--method "cardWithdraw(string,account,account,asset,byte[32])void" \
+	--arg '"Master Card"' \
+	--arg ${CARD_ADDR} \
+	--arg ${ACCT2} \
+	--arg ${FUSDC} \
+	--arg \"${WITHDRAWAL_HASH1}\" \
+	--box "b64:ACILghwHcB9gaGvkDg56T+n/iWuorpF01uMYR4jgQ6PbNQALTWFzdGVyIENhcmQ=" \
+	--fee 2000
+
+# Owner withdraws remaining 4 FUSDC
+${GOAL} app method \
+	--app-id ${APP_ID} \
+	-f ${ACCT2} \
+	--method "cardWithdraw(string,account,account,asset,byte[32])void" \
+	--arg '"Master Card"' \
+	--arg ${CARD_ADDR} \
+	--arg ${ACCT2} \
+	--arg ${FUSDC} \
+	--arg \"${WITHDRAWAL_HASH2}\" \
+	--box "b64:ACILghwHcB9gaGvkDg56T+n/iWuorpF01uMYR4jgQ6PbNQALTWFzdGVyIENhcmQ=" \
+	--fee 2000
+
+# Admin closes the card down
+# First CloseOut of FUSDC
+${GOAL} app method \
+	--app-id ${APP_ID} \
+	-f ${ACCT1} \
+	--method "cardRemoveAsset(string,account,asset)void" \
+	--arg '"Master Card"' \
+	--arg ${CARD_ADDR} \
+	--arg ${FUSDC} \
+	--fee 3000
+# Now close the card
+${GOAL} app method \
+	--app-id ${APP_ID} \
+	-f ${ACCT1} \
+	--method "cardClose(string,address,account)void" \
+	--arg '"Master Card"' \
+	--arg \"${ACCT2}\" \
+	--arg ${CARD_ADDR} \
+	--box "b64:ACILghwHcB9gaGvkDg56T+n/iWuorpF01uMYR4jgQ6PbNQALTWFzdGVyIENhcmQ=" \
+	--fee 3000
+
+# Destroy app. No more active cards
+${GOAL} app method \
+	--app-id ${APP_ID} \
+	-f ${ACCT1} \
+	--method "destroy()void" \
+	--on-completion "DeleteApplication" \
+	--fee 2000
