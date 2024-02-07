41f413ed863c3c52c29adf3055a1f856e6e82f34
nullun <git@nullun.com>
2024-02-07 17:56:59 +0000

    Rename to depositor

--- src/Immersve.algo.ts
+++ src/Immersve.algo.ts
@@ -1,9 +1,9 @@
 import { Contract } from '@algorandfoundation/tealscript';
 
-// Key to use per partner and owner (user)
-type PartnerAndOwner = {
+// Key to use per partner and depositor
+type PartnerAndDepositor = {
     partner: string,
-    owner: Address
+    depositor: Address
 };
 
 // Withdrawal request for an amount of an asset, where the round indicates the earliest it can be made
@@ -14,7 +14,7 @@ type WithdrawalRequest = {
     amount: uint64
 };
 
-// Cost of storing (partner + owner) + card in a box
+// Cost of storing (partner + depositor) + card in a box
 //const box_mbr = (2500) + (400 * (64 + 32));
 const box_mbr = 40900;
 
@@ -36,11 +36,13 @@ class Card extends Contract {
 }
 
 class Immersve extends Contract {
+
+	// ========== Storage ==========
     // Admin
     admin = GlobalStateKey<Address>({ key: 'a' });
 
-    // Owner and Card
-    cards = BoxMap<PartnerAndOwner, Address>({});
+    // Depositor and Card
+    cards = BoxMap<PartnerAndDepositor, Address>({});
     active_cards = GlobalStateKey<uint64>({ key: 'c' });
 
     // Rounds to wait
@@ -50,23 +52,31 @@ class Immersve extends Contract {
     withdrawals = LocalStateMap<bytes32, WithdrawalRequest>({ maxKeys: 15});
     withdrawal_nonce = LocalStateKey<uint64>({ key: 'n' });
 
+
+	// ========== Events ==========
+	// TODO
+
+
+	// ========== Internal Utils ==========
     /**
      * Check if the current transaction sender is the admin
      * @returns True if the sender is the admin
      */
-    private is_admin(): boolean {
+    private isAdmin(): boolean {
         return this.txn.sender === this.admin.value;
     }
 
     /**
-     * Check if the current transaction sender is the owner of the card account
+     * Check if the current transaction sender is the depositor of the card account
      * @param card Address to check
-     * @returns True if the sender is the owner of the card
+     * @returns True if the sender is the depositor of the card
      */
-    private is_owner(partner: string, card: Address): boolean {
-        return this.cards({partner: partner, owner: this.txn.sender} as PartnerAndOwner).value === card;
+    private isOwner(partner: string, card: Address): boolean {
+        return this.cards({partner: partner, depositor: this.txn.sender} as PartnerAndDepositor).value === card;
     }
 
+
+	// ========== External Methods ==========
     /**
      * Deploy the smart contract, setting the transaction sender as the admin
      */
@@ -80,7 +90,7 @@ class Immersve extends Contract {
      */
     @allow.call("UpdateApplication")
     update(): void {
-        assert(this.is_admin());
+        assert(this.isAdmin());
     }
 
     /**
@@ -88,7 +98,7 @@ class Immersve extends Contract {
      */
     @allow.call("DeleteApplication")
     destroy(): void {
-        assert(this.is_admin());
+        assert(this.isAdmin());
 
         // There must not be any active cards
         assert(!this.active_cards.value);
@@ -105,7 +115,7 @@ class Immersve extends Contract {
      * @param admin Address to be made admin
      */
     setAdmin(admin: Address): void {
-        assert(this.is_admin());
+        assert(this.isAdmin());
 
         this.admin.value = admin;
     }
@@ -115,18 +125,18 @@ class Immersve extends Contract {
      * @param rounds New number of rounds to wait
      */
     setWithdrawalRounds(rounds: uint64): void {
-        assert(this.is_admin());
+        assert(this.isAdmin());
 
         this.withdrawal_wait_time.value = rounds;
     }
 
     /**
      * Create account. This generates a brand new account and funds the minimum balance requirement
-     * @param owner Address to have control over asset withdrawals
+     * @param depositor Address to have control over asset withdrawals
      * @returns Newly generated account used by their card
      */
-    cardCreate(mbr: PayTxn, partner: string, owner: Address): Address {
-        assert(this.is_admin());
+    cardCreate(mbr: PayTxn, partner: string, depositor: Address): Address {
+        assert(this.isAdmin());
 
         assert(mbr.amount === globals.minBalance + box_mbr);
         assert(mbr.receiver === this.app.address);
@@ -145,8 +155,8 @@ class Immersve extends Contract {
             amount: globals.minBalance,
         });
 
-        // Store new card along with owner
-        this.cards({ partner: partner, owner: owner} as PartnerAndOwner).value = card_addr;
+        // Store new card along with depositor
+        this.cards({ partner: partner, depositor: depositor} as PartnerAndDepositor).value = card_addr;
 
         // Increment active cards
         this.active_cards.value = this.active_cards.value + 1;
@@ -158,11 +168,11 @@ class Immersve extends Contract {
     /**
      * Close account. This permanently removes the rekey and deletes the account from the ledger
      * @param partner Partner name
-     * @param owner Address which has control over asset withdrawals
+     * @param depositor Address which has control over asset withdrawals
      * @param card Account to close
      */
-    cardClose(partner: string, owner: Address, card: Account): void {
-        assert(this.is_admin());
+    cardClose(partner: string, depositor: Address, card: Account): void {
+        assert(this.isAdmin());
 
         sendPayment({
             sender: card,
@@ -177,20 +187,20 @@ class Immersve extends Contract {
         })
 
         // Delete the card from the box
-        this.cards({partner: partner, owner: owner} as PartnerAndOwner).delete();
+        this.cards({partner: partner, depositor: depositor} as PartnerAndDepositor).delete();
 
         // Decrement active cards
         this.active_cards.value = this.active_cards.value - 1;
     }
 
     /**
-     * Allows the owner (or admin) to OptIn to an asset, increasing the minimum balance requirement of the account
+     * Allows the depositor (or admin) to OptIn to an asset, increasing the minimum balance requirement of the account
      * @param partner Partner name
      * @param card Account to add asset to
      * @param asset Asset to add
      */
     cardAddAsset(mbr: PayTxn, partner: string, card: Account, asset: Asset): void {
-        assert(this.is_admin() || this.is_owner(partner, card));
+        assert(this.isAdmin() || this.isOwner(partner, card));
 
         assert(mbr.amount === globals.assetOptInMinBalance);
         assert(mbr.receiver === this.app.address);
@@ -209,13 +219,13 @@ class Immersve extends Contract {
     }
 
     /**
-     * Allows the owner (or admin) to CloseOut of an asset, reducing the minimum balance requirement of the account
+     * Allows the depositor (or admin) to CloseOut of an asset, reducing the minimum balance requirement of the account
      * @param partner Partner name
      * @param card Account to remove asset from
      * @param asset Asset to remove
      */
     cardRemoveAsset(partner: string, card: Account, asset: Asset): void {
-        assert(this.is_admin() || this.is_owner(partner, card));
+        assert(this.isAdmin() || this.isOwner(partner, card));
 
         sendAssetTransfer({
             sender: card,
@@ -240,7 +250,7 @@ class Immersve extends Contract {
      * @param amount Amount to debit
      */
     cardDebit(card: Account, recipient: Account, asset: Asset, amount: uint64): void {
-        assert(this.is_admin());
+        assert(this.isAdmin());
 
         sendAssetTransfer({
             sender: card,
@@ -251,7 +261,7 @@ class Immersve extends Contract {
     }
 
     /**
-     * Allows the owner to send an amount of assets from the account
+     * Allows the depositor to send an amount of assets from the account
      * @param partner Partner name
      * @param card Account to withdraw from
      * @param asset Asset being withdrawn
@@ -261,7 +271,7 @@ class Immersve extends Contract {
     @allow.call("NoOp")
     @allow.call("OptIn")
     cardWithdrawalRequest(partner: string, card: Account, asset: Asset, amount: uint64): bytes32 {
-        assert(this.is_owner(partner, card));
+        assert(this.isOwner(partner, card));
 
         const withdrawal: WithdrawalRequest = {
             nonce: this.withdrawal_nonce(this.txn.sender).value,
@@ -281,19 +291,19 @@ class Immersve extends Contract {
     }
 
     /**
-     * Allows the owner (or admin) to cancel a withdrawal request
+     * Allows the depositor (or admin) to cancel a withdrawal request
      * @param partner Partner name
      * @param card Account to withdraw from
      * @param withdrawal_hash Hash of the withdrawal request
      */
     cardWithdrawalCancel(partner: string, card: Account, withdrawal_hash: bytes32): void {
-        assert(this.is_admin() || this.is_owner(partner, card));
+        assert(this.isAdmin() || this.isOwner(partner, card));
 
         this.withdrawals(this.txn.sender, withdrawal_hash).delete();
     }
 
     /**
-     * Allows the owner to send an amount of assets from the account
+     * Allows the depositor to send an amount of assets from the account
      * @param partner Partner name
      * @param card Account to withdraw from
      * @param recipient Receiver of the assets being withdrawn
@@ -303,7 +313,7 @@ class Immersve extends Contract {
     @allow.call("NoOp")
     @allow.call("CloseOut")
     cardWithdraw(partner: string, card: Account, recipient: Account, asset: Asset, withdrawal_hash: bytes32): void {
-        assert(this.is_owner(partner, card));
+        assert(this.isOwner(partner, card));
 
         const withdrawal = this.withdrawals(this.txn.sender, withdrawal_hash).value;
 
