46c3a5c25caf32297d1140ff626331dc0da71b5c
Nathan Jones <nathan@ncjones.com>
2024-07-06 09:12:38 +1200

    Add client ref field to Card Fund

--- contracts/imsv-funding/Master.algo.ts
+++ contracts/imsv-funding/Master.algo.ts
@@ -15,6 +15,7 @@ type CardFundData = {
     address: Address;
     paymentNonce: uint64;
     withdrawalNonce: uint64;
+    ref: string;
 };
 
 type PartnerChannelData = {
@@ -503,12 +504,13 @@ export class Master extends Contract.extend(Ownable, Pausable) {
     /**
      * Retrieves the minimum balance requirement for creating a card fund account.
      * @param asset Asset to opt-in to. 0 = No asset opt-in
+     * @param ref client reference to store on the Card Fund.
      * @returns Minimum balance requirement for creating a card fund account
      */
-    getCardFundMbr(asset: AssetID): uint64 {
+    getCardFundMbr(asset: AssetID, ref: string): uint64 {
         // TODO: Double check size requirement is accurate. The prefix doesn't seem right.
         // Card Fund Data Box Cost: 2500 + 400 * (Prefix + Address + (partnerChannel + owner + address + nonce + withdrawalNonce))
-        const cardFundDataBoxCost = 2500 + 400 * (3 + 32 + (32 + 32 + 32 + 8 + 8));
+        const cardFundDataBoxCost = 2500 + 400 * (3 + 32 + (32 + 32 + 32 + 8 + 8) + len(ref));
         // Partner Card Fund Owner Box Cost: 2500 + 400 * (Prefix + hashed key(32 bytes) + cardFundAddress)
         const partnerCardFundOwnerBoxCost = 2500 + 400 * (2 + 32 + 32);
 
@@ -522,9 +524,10 @@ export class Master extends Contract.extend(Ownable, Pausable) {
      * @param mbr Payment transaction of minimum balance requirement
      * @param partnerChannel Funding Channel name
      * @param asset Asset to opt-in to. 0 = No asset opt-in
+     * @param ref client reference to store on the Card Fund.
      * @returns Newly generated account used by their card
      */
-    cardFundCreate(mbr: PayTxn, partnerChannel: Address, asset: AssetID): Address {
+    cardFundCreate(mbr: PayTxn, partnerChannel: Address, asset: AssetID, ref: string): Address {
         assert(this.partnerChannels(partnerChannel).exists, 'PARTNER_CHANNEL_NOT_FOUND');
         const partnerCardFundOwnerKeyData: PartnerCardFundData = {
             partnerChannel: partnerChannel,
@@ -539,11 +542,12 @@ export class Master extends Contract.extend(Ownable, Pausable) {
             address: globals.zeroAddress,
             paymentNonce: 0,
             withdrawalNonce: 0,
+            ref: ref,
         };
 
         verifyPayTxn(mbr, {
             receiver: this.app.address,
-            amount: this.getCardFundMbr(asset),
+            amount: this.getCardFundMbr(asset, ref),
         });
 
         // Create a new account
